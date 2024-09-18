2e4d1969493974600dc4c5e60981ee94afea0965
unknown <gnesis.vss@gmail.com>
2024-09-18 20:39:31 -0300

    fixed configurations for netlify

--- functions/server.js
+++ functions/server.js
@@ -1,4 +1,19 @@
 // functions/server.js
-const server = require('../dist/main');  // O la ruta donde se compila tu `main.ts`
+const { createServer } = require('http');
+const { handler } = require('../dist/main'); // Asegúrate de que esta ruta sea correcta
 
-module.exports.handler = server.handler;
+const server = createServer((req, res) => {
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    });
+    res.end();
+    return;
+  }
+  
+  handler(req, res);
+});
+
+module.exports.handler = server;
--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: configService.get<string>('CORS_ORIGIN'),
+    origin: configService.get<string>('CORS_ORIGIN') || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
